17af042e703d910529dad056ba14fb0201269cfe
daviskb <davis.kingwell.brown@gmail.com>
2022-08-29 21:58:59 -0700

    Completed Bot

--- main.py
+++ main.py
@@ -1,86 +1,17 @@
 import os
 import discord
+from discord.ext import commands
 
-
-class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.role_message_id = 1013340925591818311  # ID of the message that can be reacted to to add/remove a role.
-        self.emoji_to_role = {
-            discord.PartialEmoji(name='🔴'): 1013301538774597682,
-            discord.PartialEmoji(name='🟡'): 0,  # ID of the role associated with unicode emoji '🟡'.
-            discord.PartialEmoji(name='green', id=0): 0,  # ID of the role associated with a partial emoji's ID.
-        }
-
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        """Gives a role based on a reaction emoji."""
-        # Make sure that the message the user is reacting to is the one we care about.
-        if payload.message_id != self.role_message_id:
-            return
-
-        guild = self.get_guild(payload.guild_id)
-        if guild is None:
-            # Check if we're still in the guild and it's cached.
-            return
-
-        try:
-            role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-            # If the emoji isn't the one we care about then exit as well.
-            return
-
-        role = guild.get_role(role_id)
-        if role is None:
-        
-            return
-
-        try:
-          
-            await payload.member.add_roles(role)
-        except discord.HTTPException:
-			
-			pass
-            
-
-async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        """Removes a role based on a reaction emoji."""
-
-        if payload.message_id != self.role_message_id:
-            return
-
-        guild = self.get_guild(payload.guild_id)
-        if guild is None:
-
-            return
-
-        try:
-            role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-
-            return
-
-        role = guild.get_role(role_id)
-        if role is None:
-       
-            return
-
- 
-        member = guild.get_member(payload.user_id)
-        if member is None:
-    
-            return
-
-        try:
-   
-            await member.remove_roles(role)
-        except discord.HTTPException:
- 
-            pass
-
-
-intents = discord.Intents.default()
-intents.members = True
 my_secret = os.environ['TOKEN']
-client = MyClient(intents=intents)
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='$', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    guilds = [guild async for guild in client.fetch_guilds(limit=150)]
+    print(f'{guilds}')
+    await client.load_extension('messages')
+    await client.load_extension('reactions')
+
 client.run(my_secret)
